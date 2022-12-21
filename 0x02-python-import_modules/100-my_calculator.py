fc938a7b99392e73861fdc0bd51f69a6db68d839
wiphodfx <wifidifx@gmail.com>
2022-12-20 20:53:59 -0800

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-
-
-def arg_calc(argv):
-    n = len(argv) - 1
-    if n != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    a = int(argv[1])
-    op = argv[2]
-    b = int(argv[3])
-    if op == '+':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, add(a, b)))
-    elif op == '-':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, sub(a, b)))
-    elif op == '*':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, mul(a, b)))
-    elif op == '/':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, div(a, b)))
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
     import sys
-    arg_calc(sys.argv)
+    args = sys.argv
+    operators = {"+": add, "-": sub, "*": mul, "/": div}
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    if args[2] not in "+-*/":
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    a = int(args[1])
+    operator = args[2]
+    b = int(args[3])
+    c = operators[args[2]](a, b)
+    print("{:d} {:s} {:d} = {:d}".format(a, operator, b, c))
