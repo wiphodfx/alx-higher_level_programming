038d6be771230ea5a6a68e694a95c74e3915a1fd
wifidifx <wifidifx@gmail.com>
2023-03-13 13:32:44 +0000

    2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+function myVar(name){
+	if (name === " "){
+		console.log("No argument");
+	} else if(name === name){
+		console.log("Argument found");
+	} else {
+		console.log("Argument found");
+	}
+}
+myVar();
