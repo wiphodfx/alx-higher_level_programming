2d30d9eaacba61f69bd616be8683caf14ccff234
wiphodfx <wifidifx@gmail.com>
2023-03-13 14:40:17 +0100

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-function myVar(name){
-	if (name === " "){
-		console.log("No argument");
-	} else if(name === name){
-		console.log("Argument found");
-	} else {
-		console.log("Argument found");
-	}
+const argsLen = process.argv.length;
+if (argsLen === 2) {
+  console.log('No argument');
+} else if (argsLen === 3) {
+  console.log('Argument found');
+} else if (argsLen > 3) {
+  console.log('Arguments found');
 }
-myVar();
